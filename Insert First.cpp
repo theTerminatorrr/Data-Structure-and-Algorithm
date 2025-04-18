8a5fbff31561a92d68579d3db41ec2b58a164c1f
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2025-04-18 23:19:24 +0600

    Add files via upload

---
+++ Insert First.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public :
+        int value;
+        Node* next;
+    Node (int value)
+    {
+        this -> value = value;
+        this -> next = NULL;
+    }
+};
+
+void insert_First(Node* &head, int value)
+{
+   Node* newNode = new Node(value) ;
+   newNode -> next = head;
+   head = newNode;
+}
+
+void print_List(Node* head)
+{
+    while (head != NULL)
+    {
+        cout << head -> value << endl;
+        head = head -> next;
+    }
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+
+    head -> next = a;
+    a -> next = b;
+
+    insert_First(head, 100);
+    insert_First(head, 150);
+    insert_First(head, 200);
+
+    print_List(head);
+    return 0;
+
+}
